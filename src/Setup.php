8f6a8d2150c239a881cd3bd283f279dff6c2d3ce
Will Voelcker <wvoelcker@yahoo.co.uk>
2015-12-01 19:14:32 +0000

    Ensure controllers and templates directories present on update / install

---
+++ src/Setup.php
@@ -0,0 +1,39 @@
+<?php
+namespace WillV\Project;
+use Composer\Script\Event;
+
+/**
+ * This class should be hooked into composer to set-up the
+ * appropriate directories etc on update and install
+ *
+ */
+abstract class Setup {
+
+	private static function getProjectRoot($event) {
+		$projectRoot = dirname($event->getComposer()->getConfig()->get('vendor-dir'));
+	}
+
+	private static function ensureProjectDirectoryPresent($event, $relativeDirectoryPath) {
+		$projectRoot = dirname($event->getComposer()->getConfig()->get('vendor-dir'));
+		$fullDirectoryPath = $projectRoot."/".$relativeDirectoryPath;
+		
+		if (file_exists($fullDirectoryPath)) {
+			return true;
+		}
+
+		if (is_file($fullDirectoryPath)) {
+			throw new Exception("Could not create directory '".$fullDirectoryPath."' - there is already a file at that location");
+		}
+
+		mkdir($fullDirectoryPath);
+	}
+
+	public static function ensureControllersDirectoryPresent(Event $event) {
+		self::ensureProjectDirectoryPresent($event, "controllers");
+	}
+
+	public static function ensureTemplatesDirectoryPresent(Event $event) {
+		self::ensureProjectDirectoryPresent($event, "templates");
+	}
+
+}
