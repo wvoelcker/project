b8ba4fb4d36370bb8d161f3c3e0e493ab2610c72
Will Voelcker <wvoelcker@yahoo.co.uk>
2015-12-01 20:01:04 +0000

    Fixing static / instance relating errors in templateEngine

--- src/TemplateEngine.php
+++ src/TemplateEngine.php
@@ -18,12 +18,12 @@ namespace WillV\Project;
  * }
  */
 class TemplateEngine {
-	protected $projectRoot, $templatesDirectory = "templates";
+	static protected $templatesDirectory = "templates";
 
 	static public function create($projectRoot) {
 
-		$mustache = new Mustache_Engine(array(
-			"loader" => new Mustache_Loader_FilesystemLoader($projectRoot."/".$this->templatesDirectory),
+		$mustache = new \Mustache_Engine(array(
+			"loader" => new \Mustache_Loader_FilesystemLoader($projectRoot."/".static::$templatesDirectory),
 		));
 
 		return $mustache;
