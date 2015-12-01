f9469a6683575a7d852b8c69f4832e3eaf143174
Will Voelcker <wvoelcker@yahoo.co.uk>
2015-12-01 20:27:42 +0000

    Removing incorrect comment

--- src/TemplateEngine.php
+++ src/TemplateEngine.php
@@ -1,22 +1,6 @@
 <?php
 namespace WillV\Project;
 
-
-/**
- * This class should be extended within a particular project, filling in the addRoutes
- * method with appropriate routing logic.  For example:
- *
- * class ProjectRouter extends Router {
- *
- *	protected function addRoutes() {
- *
- *		$this->routeCollector->get("/", function() {
- *			$this->runController("home", func_get_args());		
- *		});
- *
- *	}
- * }
- */
 class TemplateEngine {
 	static protected $templatesDirectory = "templates";
 
