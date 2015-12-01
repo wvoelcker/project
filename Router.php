d9ba3b1e5a345104e35b13e2bc739b570edd4f3b
Will Voelcker <wvoelcker@yahoo.co.uk>
2015-12-01 18:19:40 +0000

    Initial commit

---
+++ Router.php
@@ -0,0 +1,68 @@
+<?php
+namespace WillV\Project;
+
+use Phroute\Phroute\RouteCollector;
+use Phroute\Phroute\Dispatcher;
+use Phroute\Phroute\Exception\HttpRouteNotFoundException;
+
+
+/**
+ * This class should be extended within a particular project, filling in the addRoutes
+ * method with appropriate routing logic.  For example:
+ *
+ * class ProjectRouter extends Router {
+ *
+ *	protected function addRoutes() {
+ *
+ *		$this->routeCollector->get("/", function() {
+ *			$this->runController("home", func_get_args());		
+ *		});
+ *
+ *	}
+ * }
+ */
+abstract class Router {
+	protected $projectRoot, $routeCollector, $controllersDirectory = "controllers";
+
+	protected function runController($controllerPath, $urlParams) {
+		require $this->projectRoot."/".$this->controllersDirectory."/".$controllerPath.".php";
+	}
+
+	static public function create($projectRoot) {
+		$router = new ProjectRouter;
+		$router->projectRoot = $projectRoot;
+		$router->routeCollector = new RouteCollector();
+		$router->addRoutes();
+
+		return $router;
+	}
+
+	public function go($method, $urlPath) {
+		$dispatcher = new Dispatcher($this->getRoutingData());
+
+		try {
+			return $dispatcher->dispatch($method, parse_url($urlPath, PHP_URL_PATH));
+
+		} catch (HttpRouteNotFoundException $e) {
+
+			// TODO:WV:20151201:Pass in $e
+			$this->runController("404");
+
+		} catch (Exception $e) {
+
+			// TODO:WV:20151201:Pass in $e
+			$this->runController("500");
+		}
+
+	}
+
+	protected function getRoutingData() {
+
+		// Comment from the examples in the Phroute docs:
+		// NB. You can cache the return value from $router->getData() so you don't have to create the routes each request - massive speed gains
+		return $this->routeCollector->getData();
+	}
+
+	abstract protected function addRoutes();
+
+}
