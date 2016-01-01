eade974e2040a3a5afa2dbc9b14933bc9cc541b9
Will Voelcker <wvoelcker@yahoo.co.uk>
2016-01-01 19:05:03 +0000

    Improving inline docs for the new viewConfigurator class

--- src/ViewConfigurator.php
+++ src/ViewConfigurator.php
@@ -1,5 +1,31 @@
 <?php
 namespace WillV\Project;
+/**
+ * This can be overridden to add config (template variables etc.) to all views in a project,
+ * and to specific views whenever they are used.
+ *
+ *
+ * E.g.
+ *
+ * class ProjectViewConfigurator extends ViewConfigurator {
+ *
+ *	protected function addConfigs() {
+ *		$this->configs["page"] = function($view) {
+ *			$view->set("header-carousel-images", array("image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg"));
+ *		};
+ *	}
+ * }
+ *
+ * And then in the main controller / include file:
+ *
+ * // Set up views
+ * View::setDefaultProjectRoot($projectRoot);
+ * require_once $projectRoot."/project-config/ProjectViewConfigurator.php";
+ * View::setViewConfigurator(ProjectViewConfigurator::create());
+ *
+ * 
+ */
+
 
 abstract class ViewConfigurator {
 	protected $globalConfig, $configs = array();
