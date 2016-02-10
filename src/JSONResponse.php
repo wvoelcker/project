fd60664b0ec172d7292b8550af8958f35668cbd4
Will Voelcker <wvoelcker@yahoo.co.uk>
2016-02-10 11:52:01 +0000

    Adding JSON response and validator objects

---
+++ src/JSONResponse.php
@@ -0,0 +1,44 @@
+<?php
+namespace WillV\Project;
+
+class JSONResponse {
+	private $statuscode, $data, $headersets = array();
+	private $validStatusCodesWithMessages = array(
+		200 => "OK",
+		400 => "Bad Request",
+		500 => "Internal Server Error",
+	);
+
+	static public function create($data, $statuscode = 200) {
+		$response = new JSONResponse;
+
+		if (!isset($response->validStatusCodesWithMessages[$statuscode])) {
+			throw new \Exception("Invalid status code");
+		}
+
+		$response->data = $data;
+		$response->statuscode = $statuscode;
+
+		return $response;
+	}
+
+	static public function createFromValidationErrors($validationErrors) {
+		return JSONResponse::create(array("status" => "validationError", "errors" => $validationErrors), 400);
+	}
+
+	public function addHeaders($headerset) {
+		$this->headersets[] = $headerset;
+
+		return $this;
+	}
+
+	public function send() {
+		header("HTTP/1.1 ".$this->statuscode." ".$this->validStatusCodesWithMessages[$this->statuscode]);
+		header("Content-type: application/json");
+		foreach ($this->headersets as $headerset) {
+			$headerset->send();
+		}
+		echo json_encode($this->data);
+		exit;
+	}
+}
---
+++ src/Validator.php
@@ -0,0 +1,81 @@
+<?php
+namespace WillV\Project;
+
+class Validator {
+	protected $fields;
+
+	public function create($fields) {
+		$validator = new Validator;
+		$validator->fields = $fields;
+		return $validator;
+	}
+
+	/**
+	 * Private constructor, to enforce use of factory methods
+	 **/
+	private function __construct() {}
+
+	public function setFields($fields) {
+		$this->fields = $fields;
+		return $this;
+	}
+
+	public function isValid($data, &$errors = null) {
+		$foundErrors = array();
+
+		if (empty($this->fields)) {
+			throw new Exception("No valid definitions have been supplied");
+		}
+
+		// For some reason, defaulting to an array doesn't work as expected (instead, the variable defaults to null).
+		// So default to null explicitly and set it to an array here.
+		// TODO:WV:20160210:Get to the bottom of this.
+		if (empty($errors)) {
+			$errors = array();
+		}
+
+		foreach ($this->fields as $fieldname => $fielddetails) {
+
+			if (!isset($data[$fieldname])) {
+				if (!empty($fielddetails["required"])) {
+					$foundErrors[$fielddetails] = "This field is required";
+				}
+				continue;
+			}
+
+			if (empty($data[$fieldname])) {
+				if (!empty($fielddetails["notempty"])) {
+					$foundErrors[$fielddetails] = "This field should not be empty";
+				}
+				continue;
+			}
+
+			if (!empty($fielddetails["allowedValues"]) and !in_array($data[$fieldname], $fielddetails["allowedValues"])) {
+				$foundErrors[$fielddetails] = "This field should have one of the following values: {".join(", ", $fielddetails["allowedValues"])."}";
+			}
+
+			if (!empty($fielddetails["customValidation"])) {
+				$result = call_user_func_array($fielddetails["customValidation"], array($data[$fieldname]));
+
+				if ($result === true) {
+					continue;
+				}
+
+				if (is_string($result)) {
+					$errmsg = $result;
+				} else {
+					$errmsg = "This field is invalid";
+				}
+				
+				$foundErrors[$fieldname] = $errmsg;
+			}
+		}
+
+		// Merge any validation errors found into the 'errors' array
+		$errors = $foundErrors + $errors;
+
+		// Return a boolean flag to show the result of the validation
+		return empty($foundErrors);
+	}
+
+}
