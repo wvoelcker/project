082eddc1186bea58287330dd676a1c1b158cfa39
Will Voelcker <wvoelcker@yahoo.co.uk>
2016-05-05 21:20:01 +0100

    Allowing logging of PDO queries for optimisation etc

--- src/PDOGenerator.php
+++ src/PDOGenerator.php
@@ -3,6 +3,7 @@ namespace WillV\Project;
 
 class PDOGenerator {
 	protected $pdo, $hostname = null, $databasename = null, $username = null, $password = null;
+	protected $logger;
 
 	static public function create($hostname, $databasename, $username, $password) {
 		$db = new PDOGenerator;
@@ -16,16 +17,38 @@ class PDOGenerator {
 	private function __construct() {
 	}
 
+
+	// The following function expects a PEAR logger
+	public function setLogger(\Log $logger) {
+		$this->logger = $logger;
+	}
+
 	public function getPDO() {
 		if (($this->hostname === null) or ($this->databasename === null) or ($this->username === null) or ($this->password === null)) {
 			throw new \Exception("Can't generate a PDO without a hostname, databasename, username, and password");
 		}
 
+		$connectionString = "mysql:host=".$this->hostname.";dbname=".$this->databasename.";charset=utf8";
+
+		if (!empty($this->logger)) {
+			return new \LoggedPDO\PDO(
+				$connectionString,
+				$this->username,
+				$this->password,
+				array(
+					\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+				),
+				$logger
+			);
+		}
+
 		return new \pdo(
-			"mysql:host=".$this->hostname.";dbname=".$this->databasename.";charset=utf8",
+			$connectionString,
 			$this->username,
 			$this->password,
-			array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION)
+			array(
+				\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+			)
 		);
 	}
 }
