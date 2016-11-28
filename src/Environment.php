a8bab31130ee4bbd6168c44d64c29a537964c3f6
Will Voelcker <wvoelcker@yahoo.co.uk>
2016-11-28 17:56:34 +0000

    Allowing data-files to be specified outside the main environment definition (e.g. so that they can be excluded from version control)

--- src/Environment.php
+++ src/Environment.php
@@ -6,11 +6,38 @@ abstract class Environment {
 	protected $data, $isActiveClosure, $requiredFields;
 
 	protected function postSetUp() {
-		list($data, $isActiveClosure) = func_get_args();
+
+		// Unpack arguments
+		$args = func_get_args();
+		$numArgs = count($args);
+		switch ($numArgs) {
+			case 2:
+				list($data, $isActiveClosure) = $args;
+				break;
+			case 3:
+				list($data, $dataFiles, $isActiveClosure) = $args;
+				break;
+			default:
+				throw new \Exception("Unexpected number of arguments");
+		}
+
+		// Extract extra data from data-files if any were specified
+		if (!empty($dataFiles)) {
+			foreach ($dataFiles as $dataFile) {
+				if (!file_exists($dataFile)) {
+					throw new \Exception("Data file not found: ".$dataFile);
+				}
+				$data += json_decode(file_get_contents($dataFile));
+			}
+		}
+
+		// Make sure all required data was provided
 		$notSupplied = array_diff($this->requiredFields, array_keys($data));
 		if (!empty($notSupplied)) {
 			throw new \Exception("Missing fields: {".join(", ", $notSupplied)."}");
 		}
+
+		// Populate class properties
 		$this->data = $data;
 		$this->isActiveClosure = $isActiveClosure;
 	}
