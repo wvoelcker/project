2cd3789e98b52f293f8694b37d78df98d52f2cca
Will Voelcker <wvoelcker@yahoo.co.uk>
2016-11-30 11:03:54 +0000

    Adding more sophisticated validation to domain-object class

--- src/DomainObject.php
+++ src/DomainObject.php
@@ -3,14 +3,24 @@ namespace WillV\Project;
 
 class DomainObject {
 	use Trait_AbstractTemplate;
-	protected $data = array(), $requiredFields = array();
+	protected $data = array(), $fields = array();
 
 	protected function postSetUp() {
 		$data = func_get_arg(0);
-		$notSupplied = array_diff($this->requiredFields, array_keys($data));
-		if (!empty($notSupplied)) {
-			throw new \Exception("Missing fields: {".join(", ", $notSupplied)."}");
+
+		$validationErrors = array();
+		foreach ($this->fields as $fieldname => $fieldDetails) {
+			if (!empty($fieldDetails["required"]) and !isset($data[$fieldname])) {
+				$validationErrors[$fieldname] = "Not supplied";
+			} elseif (!empty($fieldDetails["notempty"]) and empty($data[$fieldname])) {
+				$validationErrors[$fieldname] = "Cannot be empty";
+			}
 		}
+
+		if (!empty($validationErrors)) {
+			throw new \Exception("Problems with supplied data: ".json_encode($validationErrors));
+		}
+
 		$this->data = $data;
 	}
 
