fe2dd2d14367576c536b72e0d0991b24d624b59d
Will Voelcker <wvoelcker@yahoo.co.uk>
2017-02-13 10:37:19 +0000

    Refactoring dataset to make it easier to add additional validators

--- src/Dataset.php
+++ src/Dataset.php
@@ -26,93 +26,46 @@ abstract class Dataset {
 		foreach ($this->fields as $fieldname => $fielddetails) {
 
 			// Check for empty-but-not-supposed to be (NB do this before the isset check, or "notempty" fields that are not set will pass validation)
-			if (empty($data[$fieldname])) {
-				if (!empty($fielddetails["notempty"])) {
-					$foundErrors[$fieldname] = "This field should not be empty";
-				}
-
+			if (!empty($fielddetails["notempty"]) and empty($data[$fieldname])) {
+				$foundErrors[$fieldname] = "This field should not be empty";
 				// NB no 'continue' here because 0 is an empty value, but can also be processed by the rest of this function
 			}
 
-			if (!isset($data[$fieldname])) {
-				if (!empty($fielddetails["required"])) {
-					$foundErrors[$fieldname] = "This field is required";
-				}
+			if (!empty($fielddetails["required"]) and !isset($data[$fieldname])) {
+				$foundErrors[$fieldname] = "This field is required";
 				continue;
 			}
 
-			if (!empty($fielddetails["allowedValues"]) and !in_array($data[$fieldname], $fielddetails["allowedValues"])) {
-				$foundErrors[$fieldname] = "This field should have one of the following values: {".join(", ", $fielddetails["allowedValues"])."}";
-				continue;
+			$validators = array();
+
+			if (!empty($fielddetails["allowedValues"])) {
+				$validators[] = function($value, $dataset) use ($fielddetails) {
+					if (!in_array($value, $fielddetails["allowedValues"])) {
+						return "This field should have one of the following values: {".join(", ", $fielddetails["allowedValues"])."}";
+					}
+				};
 			}
 
 			if (!empty($fielddetails["validateDateMySQL"])) {
-				$result = preg_match("/^([0-9]{4})-([0-9]{2})-([0-9]{2})$/", $data[$fieldname], $m);
-
-				if ($result == true and checkdate($m[2], $m[3], $m[1])) {
-					$errmsg = null;
-				} elseif ($result == false) {
-					$errmsg = "Not a date in the format yyyy-mm-dd";
-				} else {
-					$errmsg = "Not a valid date";
-				}
-
-				if (!empty($errmsg)) {
-					$foundErrors[$fieldname] = $errmsg;
-					continue;
-				}
+				$validators[] = array($this, "validateDateMySQL");
 			}
 
 			if (!empty($fielddetails["validateDateISO8601"])) {
-				$result = preg_match("/^([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]{2})(([+\-])([0-9]{2}):([0-9]{2}))?$/", $data[$fieldname], $m);
-
-				if ($result == false) {
-					$errmsg = "Not a date in the format yyyy-mm-ddThh:mm:ss(+/-hh:mm)";
-				} else {
-					if (empty($m[8])) {
-						$minutesOffset = 0;
-						$offsetIsPositive = true;
-					} else {
-						$minutesOffset = (($m[9] * 60) + $m[10]);
-						if ($m[8] == "-") {
-							$minutesOffset *= -1;
-						}
-					}
-
-					if (strtotime($data[$fieldname]) != gmmktime($m[4], $m[5] - $minutesOffset, $m[6], $m[2], $m[3], $m[1])) {
-						$errmsg = "Not a valid date";
-					}
-				}
-
-				if (!empty($errmsg)) {
-					$foundErrors[$fieldname] = $errmsg;
-					continue;
-				}
+				$validators[] = array($this, "validateDateISO8601");
 			}
 
 			if (!empty($fielddetails["validateEmailAddress"])) {
-				$result = filter_var($data[$fieldname], FILTER_VALIDATE_EMAIL);
-
-				if (!$result) {
-					$foundErrors[$fieldname] = "Not a valid email address";
-					continue;
-				}
+				$validators[] = array($this, "validateEmailAddress");
 			}
 
 			if (!empty($fielddetails["customValidation"])) {
-				$result = call_user_func_array($fielddetails["customValidation"], array($data[$fieldname], $data));
+				$validators[] = $fielddetails["customValidation"];
+			}
 
-				if ($result === true) {
-					$errmsg = null;
-				} elseif (is_string($result)) {
-					$errmsg = $result;
-				} else {
-					$errmsg = "This field is invalid";
-				}
-
-				if (!empty($errmsg)) {
-					$foundErrors[$fieldname] = $errmsg;
-					continue;
+			foreach ($validators as $validator) {
+				$result = $this->doValidationStep($validator, $data, $fieldname, $foundErrors);
+				if ($result === false) {
+					continue 2;
 				}
 			}
 		}
@@ -122,6 +75,73 @@ abstract class Dataset {
 
 		// Return a boolean flag to show the result of the validation
 		return empty($foundErrors);
+	}
+
+	protected function validateDateMySQL($value) {
+		$result = preg_match("/^([0-9]{4})-([0-9]{2})-([0-9]{2})$/", $value, $m);
+
+		if ($result == true) {
+			if (checkdate($m[2], $m[3], $m[1])) {
+				return true;
+			} else {
+				return "Not a valid date";
+			}
+		}
+
+		return "Not a date in the format yyyy-mm-dd";
+	}
+
+	protected function validateDateISO8601($value) {
+		$result = preg_match("/^([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]{2})(([+\-])([0-9]{2}):([0-9]{2}))?$/", $value, $m);
+
+		if ($result == false) {
+			$errmsg = "Not a date in the format yyyy-mm-ddThh:mm:ss(+/-hh:mm)";
+		} else {
+			if (empty($m[8])) {
+				$minutesOffset = 0;
+				$offsetIsPositive = true;
+			} else {
+				$minutesOffset = (($m[9] * 60) + $m[10]);
+				if ($m[8] == "-") {
+					$minutesOffset *= -1;
+				}
+			}
+
+			if (strtotime($value) != gmmktime($m[4], $m[5] - $minutesOffset, $m[6], $m[2], $m[3], $m[1])) {
+				$errmsg = "Not a valid date";
+			}
+		}
+
+		if (!empty($errmsg)) {
+			return $errmsg;
+		}
+
+		return true;
+	}
+
+	private function doValidationStep($validationFunction, $data, $fieldname, &$foundErrors) {
+		$result = call_user_func_array($validationFunction, array($data[$fieldname], $data));
+
+		if ($result === true) {
+			return true;
+		} elseif (is_string($result)) {
+			$errmsg = $result;
+		} else {
+			$errmsg = "This field is invalid";
+		}
+
+		$foundErrors[$fieldname] = $errmsg;
+		return false;
+	}
+
+	protected function validateEmailAddress($value) {
+		$result = filter_var($data[$fieldname], FILTER_VALIDATE_EMAIL);
+
+		if (!$result) {
+			return "Not a valid email address";
+		}
+
+		return true;
 	}
 
 }
