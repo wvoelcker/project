72c8c76f8eb425a110c97315a8892095560a1cc6
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-02-26 15:41:42 +0000

    Adding unit tests for the contact-list

---
+++ src/tests/ContactListTest.php
@@ -0,0 +1,81 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use WillV\Project\ContactList;
+
+class TestContactList extends ContactList {
+	protected function setUp() {
+
+		$this->addContact("testContact1", array(
+			"email" => "test1@example.com",
+			"name" => "Test Contact 1",
+		));
+
+		$this->addContact("testContact2", array(
+			"email" => "test2@example.com",
+			"name" => "Test Contact 2",
+		));
+
+		$this->addContact("testContact3", array(
+			"email" => "test3@example.com",
+			"name" => "Test Contact 3",
+		));
+	}
+}
+
+class TestBrokenContactList extends ContactList {
+	protected function setUp() {
+
+		$this->addContact("testContact1", array(
+			"email" => "test1@example.com",
+			"name" => "Test Contact 1",
+		));
+
+		$this->addContact("testContact1", array(
+			"email" => "test1@example.com",
+			"name" => "Test Contact 1",
+		));
+	}
+}
+
+class ContactListTest extends TestCase {
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage Contact 'testContact1' already added
+     */
+	public function testItShouldThrowAnExceptionIfTheSameContactAddedTwice() {
+		TestBrokenContactList::create();
+	}
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage Contact 'nonExistentContact' not found
+     */
+	public function testItShouldThrowAnExceptionIfTheContactIsNotFound() {
+		TestContactList::create()->getContact("nonExistentContact");
+	}
+
+	public function testItShouldFindAndReturnDataForTheFirstContactInTheList() {
+		$contact = TestContactList::create()->getContact("testContact1");
+		$this->assertEquals($contact, array(
+			"email" => "test1@example.com",
+			"name" => "Test Contact 1",
+		));
+	}
+
+	public function testItShouldFindAndReturnDataForTheSecondContactInTheList() {
+		$contact = TestContactList::create()->getContact("testContact2");
+		$this->assertEquals($contact, array(
+			"email" => "test2@example.com",
+			"name" => "Test Contact 2",
+		));
+	}
+
+	public function testItShouldFindAndReturnDataForTheThirdContactInTheList() {
+		$contact = TestContactList::create()->getContact("testContact3");
+		$this->assertEquals($contact, array(
+			"email" => "test3@example.com",
+			"name" => "Test Contact 3",
+		));
+	}
+}
