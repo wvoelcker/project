14bf0e97516d2b62a7afe268a111e06f718fa593
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-02-26 12:56:24 +0000

    Adding first test (for ArrayFilterer)

---
+++ src/tests/ArrayFiltererTest.php
@@ -0,0 +1,23 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use WillV\Project\ArrayFilterer;
+
+class ArrayFiltererTest extends TestCase {
+	public function testCreateShouldMakeANewArrayFilterer() {
+		$filterer = ArrayFilterer::create();
+		$this->assertTrue($filterer instanceof ArrayFilterer);
+	}
+	public function testFilterByKeyShouldFilterAnArrayByKey() {
+		$filterer = ArrayFilterer::create();
+		$inputArray = array(
+			"a" => 1,
+			"b" => 2,
+			"c" => 3,
+			"d" => 4,
+			"e" => 5,
+			"f" => 6
+		);
+		$outputArray = $filterer->filterByKey($inputArray, array("a", "c", "e"));
+		$this->assertEquals($outputArray, array("a" => 1, "c" => 3, "e" => 5));
+	}
+}
