e8931628464aa09229f33bc21ba278de5dc3cffc
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-02-27 12:54:26 +0000

    Starting the Dataset test

---
+++ src/tests/DatasetTest.php
@@ -0,0 +1,123 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use WillV\Project\Dataset;
+
+class ExampleDataset extends Dataset {
+
+	protected function setUp() {
+		$this->fields = array(
+			"this-field-cannot-be-empty" => array(
+				"notempty" => true
+			),
+			"this-field-must-be-supplied" => array(
+				"required" => true
+			),
+			"this-field-is-optional" => array(
+				"required" => false
+			),
+			"this-field-has-a-set-of-allowed-values" => array(
+				"allowedValues" => array("a", "b", "c")
+			),
+			"this-field-should-be-a-12-hour-time" => array(
+				"validate12HrTime" => true
+			),
+			"this-field-should-be-a-uk-format-date" => array(
+				"validateDateUK" => true
+			),
+			"this-field-should-be-a-mysql-format-date" => array(
+				"validateDateMySQL" => true
+			),
+			"this-field-should-be-a-iso-8601-format-date" => array(
+				"validateDateISO8601" => true
+			),
+			"this-field-should-be-an-email-address" => array(
+				"validateEmailAddress" => true
+			),
+			"this-field-has-custom-validation-to-check-if-it-is-a-digit" => array(
+				"customValidation" => "ctype_digit"
+			),
+		);
+	}
+}
+
+class ExampleDatasetWithNoFields extends Dataset {
+	protected function setUp() {}
+}
+
+class TestDataset extends TestCase {
+
+	public function testItShouldReturnAllFieldsAndNoExtraFieldsWhenGetFieldsIsCalled() {
+		$dataSet = ExampleDataset::create();
+		$fields = $dataSet->getFields();
+		$this->assertEquals(array(
+			"this-field-cannot-be-empty" => array("notempty" => true),
+			"this-field-must-be-supplied" => array("required" => true),
+			"this-field-is-optional" => array("required" => false),
+			"this-field-has-a-set-of-allowed-values" => array("allowedValues" => array("a", "b", "c")),
+			"this-field-should-be-a-12-hour-time" => array("validate12HrTime" => true),
+			"this-field-should-be-a-uk-format-date" => array("validateDateUK" => true),
+			"this-field-should-be-a-mysql-format-date" => array("validateDateMySQL" => true),
+			"this-field-should-be-a-iso-8601-format-date" => array("validateDateISO8601" => true),
+			"this-field-should-be-an-email-address" => array("validateEmailAddress" => true),
+			"this-field-has-custom-validation-to-check-if-it-is-a-digit" => array("customValidation" => "ctype_digit"),
+		), $fields);
+	}
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage No valid definitions have been supplied
+     */
+	public function testItShouldThrowAnExceptionIfIsValidIsCalledWithoutAnyFieldsBeingDefined() {
+		$dataSet = ExampleDatasetWithNoFields::create();
+		$dataSet->isValid(array(), $errors);
+	}
+
+	public function testItShouldReportIfANotEmptyFieldIsEmpty() {
+
+	}
+
+	public function testItShouldReportIfARequiredFieldIsMissing() {
+
+	}
+
+	public function testItShouldNotAttemptToValidateFieldsThatAreNotRequiredAndWereNotProvided() {
+
+	}
+
+	public function testItShouldReportIfTheFieldHasASetOfAllowedValuesAndTheSuppliedValueWasNotOneOfThem() {
+
+	}
+
+	public function testItShouldNotReportIfTheFieldHasASetOfAllowedValuesAndTheSuppliedValueWasOneOfThem() {
+		
+	}
+
+	public function testItShouldNotReportAnyMessagesAboutAllowedValuesIfTheFieldDoesNotHaveASetOfAllowedValues() {
+		
+	}
+
+	public function testItShouldValidate12HourTimes() {
+
+	}
+
+	public function testItShouldValidateUKDates() {
+
+	}
+
+	public function testItShouldValidateMySQLDates() {
+
+	}
+
+	public function testItShouldValidateISO8601Dates() {
+
+	}
+
+	public function testItShouldValidateEmailAddresses() {
+
+	}
+
+	public function testItShouldDoAnyCustomValidation() {
+
+	}
+
+}
