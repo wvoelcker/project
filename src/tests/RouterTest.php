ca100cf6c7d3884128ad7d1e3dc9a2d18f984ad4
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-03-03 19:50:08 +0000

    Adding Router Test

---
+++ src/tests/RouterTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace WillV\Project\Tests\Router;
+use PHPUnit\Framework\TestCase;
+use WillV\Project\Router;
+
+class ExampleRouter extends Router {
+	protected function setUp() {
+	}
+}
+
+class TestRouter extends TestCase {
+}
