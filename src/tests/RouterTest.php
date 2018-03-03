12b4506a85c23f02827fc498ad9e35ba107c26c6
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-03-03 19:58:25 +0000

    Setting up Router test

--- src/tests/RouterTest.php
+++ src/tests/RouterTest.php
@@ -5,6 +5,31 @@ use WillV\Project\Router;
 
 class ExampleRouter extends Router {
 	protected function setUp() {
+		$this->get("/url/1", "controller-1");
+		$this->get("/url/1/json", "controller-1", "application/json");
+		$this->get("/url/2", "controller-2");
+
+		$this->post("/url/3", "controller-3");
+		$this->post("/url/3/json", "controller-3", "application/json");
+		$this->post("/url/4", "controller-4");
+
+		$this->put("/url/5", "controller-5");
+		$this->put("/url/5/json", "controller-5", "application/json");
+		$this->put("/url/6", "controller-6");
+
+		$this->delete("/url/7", "controller-7");
+		$this->delete("/url/7/json", "controller-7", "application/json");
+		$this->delete("/url/8", "controller-8");
+
+		$this->getOrPost("/url/9", "controller-9");
+		$this->getOrPost("/url/9/json", "controller-9", "application/json");
+		$this->getOrPost("/url/9", "controller-9");
+
+
+		// Test same URL, different controller for different HTTP methods
+		$this->post("/url/1", "controller-1a");
+		$this->put("/url/1", "controller-1b");
+		$this->delete("/url/1", "controller-1c");
 	}
 }
 
