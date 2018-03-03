52de2d5d19951737e7ac45d955002af965a4ff12
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-03-03 11:42:17 +0000

    Adding tests for JSONResponse (not finished)

---
+++ src/tests/JSONResponseTest.php
@@ -0,0 +1,122 @@
+<?php
+namespace WillV\Project\Tests\JSONResponseTest;
+use PHPUnit\Framework\TestCase;
+use WillV\Project\JSONResponse;
+use WillV\Project\HTTPHeaders\HeaderSet;
+
+class ExampleHeaderSet extends HeaderSet {
+	protected function setUp() {
+		$this->headers["X-TEST-HEADER"] = "test-header-value";
+	}
+}
+
+class TestJSONResponse extends TestCase {
+	public function testItShouldNotThrowAnExceptionIfTheStatusCodeWas200() {
+		$this->confirmNoException(200);
+	}
+
+	private function confirmNoException($statusCode) {
+		$e = null;
+		try {
+			$response = JSONResponse::create(array("testKey" => "testValue"), $statusCode);	
+		} catch (\Exception $e) {
+			// Do nothing
+		}
+
+		$this->assertEquals(null, $e);
+	}
+
+	public function testItShouldNotThrowAnExceptionIfTheStatusCodeWas400() {
+		$this->confirmNoException(400);
+	}
+
+	public function testItShouldNotThrowAnExceptionIfTheStatusCodeWas403() {
+		$this->confirmNoException(403);
+	}
+
+	public function testItShouldNotThrowAnExceptionIfTheStatusCodeWas404() {
+		$this->confirmNoException(404);
+	}
+
+	public function testItShouldNotThrowAnExceptionIfTheStatusCodeWas500() {
+		$this->confirmNoException(500);
+	}
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage Invalid status code
+     */
+	public function testItShouldThrowAnExceptionIfThesStatusCodeWasInvalid() {
+		$response = JSONResponse::create(array("testKey" => "testValue"), 418);	
+	}
+
+	public function testItShouldReturnAJSONResponseObjectWhenCreateIsCalled() {
+		$response = JSONResponse::create(array("testKey" => "testValue"), 200);
+		$this->assertTrue($response instanceof JSONResponse);
+	}
+
+    /**
+    * @runInSeparateProcess
+    */
+	public function testItShouldCreateAnObjectWithStatusValidationErrorWhenCallingCreateFromValidationErrors() {
+		$response = JSONResponse::createFromValidationErrors(array("example-field" => "This field was invalid"));
+
+		ob_start();
+		$response->send();
+		$output = @json_decode(ob_get_contents());
+		ob_end_clean();
+
+		$this->assertTrue(!empty($output));
+		$this->assertTrue(is_object($output));
+		$this->assertObjectHasAttribute("status", $output);
+		$this->assertEquals("validationError", $output->status);
+	}
+
+    /**
+    * @runInSeparateProcess
+    */
+	public function testItShouldCreateAnObjectContainingTheAppropriateErrorMessagesWhenCallingCreateFromValidationErrors() {
+		$response = JSONResponse::createFromValidationErrors(array("example-field" => "This field was invalid"));
+
+		ob_start();
+		$response->send();
+		$output = @json_decode(ob_get_contents());
+		ob_end_clean();
+
+		$this->assertTrue(!empty($output));
+		$this->assertTrue(is_object($output));
+		$this->assertObjectHasAttribute("errors", $output);
+		$this->assertObjectHasAttribute("example-field", $output->errors);
+	}
+
+    /**
+    * @runInSeparateProcess
+    */
+	public function testItShouldSendTheCorrectCustomHeaders() {
+		if (!function_exists("xdebug_get_headersAA")) {
+			$this->markTestSkipped("XDebug is not available, so not able to verify headers");
+		}
+		$response = JSONResponse::createFromValidationErrors(array("example-field" => "This field was invalid"));
+		$response->addHeaders(ExampleHeaderSet::create());
+
+		ob_start();
+		$response->send();
+		ob_end_clean();
+		$headersSent = xdebug_get_headers();
+
+		$this->assertTrue(is_array($headersSent));
+		$this->assertTrue(in_array("X-TEST-HEADER: test-header-value", $headersSent));
+	}
+
+	public function testItShouldSendTheCorrectStatusCode() {
+
+	}
+
+	public function testItShouldSendADefaultStatusCodeOf200() {
+
+	}
+
+	public function testItShouldSendTheCorrectCustomData() {
+
+	}
+}
