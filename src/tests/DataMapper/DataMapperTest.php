102385a2c5fe127b7798d692a702cb5986c52dee
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-03-06 13:26:35 +0000

    Adding some initial test bodies to the datamapper test

--- src/tests/DataMapper/DataMapperTest.php
+++ src/tests/DataMapper/DataMapperTest.php
@@ -5,9 +5,49 @@ use WillV\Project\DataMapper\DataMapper;
 use WillV\Project\DomainObject;
 use WillV\Project\Dataset;
 
+
+class Item extends DomainObject {
+	protected function setUp() {
+		$this->dataSetName = __NAMESPACE__."\ItemDataset";
+	}
+}
+
+class ItemDataset extends Dataset {
+	protected function setUp() {
+		$this->fields = array(
+			"id" => array(
+				"customValidation" => "ctype_digit",
+			),
+			"size" => array(
+				"allowedValues" => array("small", "medium", "large"),
+				"required" => true,
+				"visibility" => "public",
+			),
+			"name" => array(
+				"visibility" => "public",
+			),
+			"itemId" => array(
+				"customValidation" => "is_string",
+			)
+		);
+	}
+}
+
+
 abstract class ExampleDataMapperType extends DataMapper {
 	public $gotRows = array(), $countedRows = array(), $deletedById = array();
 	public $rowsFetched = array(), $dateCreatedGot = array(), $savedSingle = array(), $savedMultiple = array();
+
+	public $testData = array();
+
+	protected function preSetUp() {
+		parent::preSetUp();
+		$this->testData = array(
+			array("id" => "1", "size" => "medium", "name" => "thing1", "item_id" => "YWJjZGVm", "created_utc" => new \DateTime("@1518048000", new \DateTimeZone("UTC"))),
+			array("id" => "2", "size" => "large", "name" => "thing2", "item_id" => "eng5ODcxYg==", "created_utc" => new \DateTime("@1518134400", new \DateTimeZone("UTC"))),
+			array("id" => "4", "size" => "small", "name" => "thing3", "item_id" => "c2Rmc2s4NzIz", "created_utc" => new \DateTime("@1518220800", new \DateTimeZone("UTC"))),
+		);
+	}
 
 	protected function getRows($sortCol, $sortDir, $offset, $maxResults, $criteria = array()) {
 		$this->gotRows[] = func_get_args();
@@ -22,7 +62,21 @@ abstract class ExampleDataMapperType extends DataMapper {
 	}
 
 	protected function fetchRow($criteria) {
-		$this->rowsFetched[] = func_get_args();
+		$filtered = $this->filter($this->testData, $criteria);
+
+		// TODO:WV:20180306:What to return if the filtered array is empty?  See also the MySQL Mapper.
+		return $filtered[0];
+	}
+
+	protected function filter($data, $criteria) {
+		return array_values(array_filter($data, function($v) use ($criteria) {
+			foreach ($criteria as $key => $value) {
+				if (!isset($v[$key]) or $v[$key] != $value) {
+					return false;
+				}
+			}
+			return true;
+		}));
 	}
 
 	protected function getDateCreatedById($id) {
@@ -67,41 +121,18 @@ class ItemMapper extends ExampleDataMapperType {
 	}
 }
 
-class Item extends DomainObject {
-	protected function setUp() {
-		$this->dataSetName = __NAMESPACE__."\ItemDataset";
-	}
-}
-
-class ItemDataset extends Dataset {
-	protected function setUp() {
-		$this->fields = array(
-			"id" => array(
-				"customValidation" => "ctype_digit",
-			),
-			"size" => array(
-				"allowedValues" => array("small", "medium", "large"),
-				"required" => true,
-				"visibility" => "public",
-			),
-			"name" => array(
-				"visibility" => "public",
-			),
-			"itemId" => array(
-				"customValidation" => "is_string",
-			)
-		);
-	}
-}
-
 class TestDataMapper extends TestCase {
 
 	public function testItShouldFindAnObjectById() {
 		$mapper = ItemMapper::create();
+		$item = $mapper->findById(2);
+		$this->assertEquals("thing2", $item->get("name"));
 	}
 
-	public function testItSHouldFindASingleObjectByCriteriaOtherThanId() {
-
+	public function testItShouldFindASingleObjectByCriteriaOtherThanId() {
+		$mapper = ItemMapper::create();
+		$item = $mapper->findSingleFromCriteria(array("size" => "small"));
+		$this->assertEquals("thing3", $item->get("name"));
 	}
 
 	public function testItShouldThrowAnExceptionIfTheSortDirectionWasInvalidWhenGeneratingAPageOfObjects() {
