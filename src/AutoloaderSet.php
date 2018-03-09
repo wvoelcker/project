095966fec96d5df8649cfa0c0f9dce45d2791d3e
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-03-09 11:15:19 +0000

    Adding default autoloader to autoloaderset

--- src/AutoloaderSet.php
+++ src/AutoloaderSet.php
@@ -1,13 +1,40 @@
 <?php
 namespace WillV\Project;
 
+// TODO:WV:20180309:Test latest changes
 abstract class AutoloaderSet {
 	use Trait_AbstractTemplate;
-	protected $projectRoot, $autoloaders = array();
+	protected $projectRoot, $rootNamespace, $namespaceDirectories;
+	protected $autoloaders = array();
 
 	protected function preSetUp() {
 		$this->projectRoot = func_get_arg(0);
+		$this->rootNamespace = func_get_arg(1);
+		$this->namespaceDirectories = func_get_arg(2);
+
+		$this->addAutoloader(function($className) {
+			$classPath = $this->getClassPath($className);
+			if (!empty($classPath) and file_exists($classPath)) {
+				require_once $classPath;
+			}
+		});
 	}
+
+	private function getClassPath($className, $mappings = array()) {
+		$classDetails = explode("\\", $className);
+		$numClassDetails = count($classDetails);
+
+		if (($numClassDetails != 3) or ($classDetails[0] != $this->rootNamespace)) {
+			return null;
+		}
+
+		if (!isset($this->namespaceDirectories[$classDetails[1]])) {
+			throw new \Exception("Unknown subnamespace '".$classDetails[1]."'");
+		}
+
+		return $this->projectRoot."/".strtolower($this->namespaceDirectories[$classDetails[1]])."/".$classDetails[2].".php";
+	}
+
 
 	protected function addAutoloader($autoloader) {
 		$this->autoloaders[] = $autoloader;
