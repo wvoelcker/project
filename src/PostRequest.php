88e56600504c5b4a1be22d6a1a246e9e185242ce
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-03-10 18:04:29 +0000

    Simplifying further

--- src/PostRequest.php
+++ src/PostRequest.php
@@ -3,11 +3,11 @@ namespace WillV\Project;
 
 class PostRequest {
 
-	static public function dataFromJSON($requestMethod, $postData, $incomingRequestBody = null) {
+	static public function dataFromJSON($postData, $incomingRequestBody = null) {
 		if ($incomingRequestBody === null) {
 			$incomingRequestBody = file_get_contents('php://input');
 		}
-		if (strtolower($requestMethod) == "post" and empty($postData)) {
+		if (empty($postData)) {
 			$incomingData = @json_decode($incomingRequestBody, true);
 
 			if (is_array($incomingData) and !empty($incomingData)) {
