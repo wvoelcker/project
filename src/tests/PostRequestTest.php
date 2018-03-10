a61b70cf95a3ab31093e7a9e77118108e9d61bcf
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-03-10 18:02:20 +0000

    Simplifying post request parser

--- src/tests/PostRequestTest.php
+++ src/tests/PostRequestTest.php
@@ -6,32 +6,27 @@ use WillV\Project\PostRequest;
 class TestPostRequest extends TestCase {
 
 	public function testItShouldReturnTheInputArrayWhenGeneratingDataFromIncomingJSONForGETRequests() {
-		$output = PostRequest::dataFromJSON("GET", array(), "application/json", "{'a':'b'}");
+		$output = PostRequest::dataFromJSON("GET", array(), "{'a':'b'}");
 		$this->assertEquals(array(), $output);
 	}
 
 	public function testItShouldReturnTheInputArrayWhenGeneratingDataFromIncomingJSONIfThereIsSomethingInThePOSTArray() {
-		$output = PostRequest::dataFromJSON("POST", array("c" => "d"), "application/json", "{'a':'b'}");
+		$output = PostRequest::dataFromJSON("POST", array("c" => "d"), "{'a':'b'}");
 		$this->assertEquals(array("c" => "d"), $output);
 	}
 
-	public function testItShouldReturnTheInputArrayWhenGeneratingDataFromIncomingJSONIfTheIncomingContentTypeIsNotApplicationJSON() {
-		$output = PostRequest::dataFromJSON("POST", array(), "text/plain", "{'a':'b'}");
-		$this->assertEquals(array(), $output);
-	}
-
 	public function testItShouldReturnTheInputArrayWhenGeneratingDataFromIncomingJSONIfTheIncomingContentBodyIsNotValidJSON() {
-		$output = PostRequest::dataFromJSON("POST", array(), "application/json", "invalid-json");
+		$output = PostRequest::dataFromJSON("POST", array(), "invalid-json");
 		$this->assertEquals(array(), $output);
 	}
 
 	public function testItShouldReturnTheInputArrayWhenGeneratingDataFromIncomingJSONIfTheIncomingContentBodyIsValidJSONButDoesNotEvaluateToAnAppropriateObjectOrArray() {
-		$output = PostRequest::dataFromJSON("POST", array(), "application/json", "'somestring'");
+		$output = PostRequest::dataFromJSON("POST", array(), "'somestring'");
 		$this->assertEquals(array(), $output);
 	}
 
 	public function testItShouldParseAValidIncomingJSONRequestAndConvertItToAnArray() {
-		$output = PostRequest::dataFromJSON("POST", array(), "application/json", json_encode(array("a" => "b")));
+		$output = PostRequest::dataFromJSON("POST", array(), json_encode(array("a" => "b")));
 		$this->assertNotNull($output);
 		$this->assertNotEmpty($output);
 		$this->assertNotEmpty($output["a"]);
