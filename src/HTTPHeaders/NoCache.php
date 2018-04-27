8ddeb84d6b980a967b83318b07ebe6076c647a68
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-04-27 21:44:21 +0100

    Add 'private' to nocache headers
    I was advised that this is necessary to prevent Fastly grouping requests together, and serving no-cache requests to multiple users.

--- src/HTTPHeaders/NoCache.php
+++ src/HTTPHeaders/NoCache.php
@@ -3,7 +3,7 @@ namespace WillV\Project\HTTPHeaders;
 
 class NoCache extends HeaderSet {
 	protected function setUp() {
-		$this->headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
+		$this->headers["Cache-Control"] = "private, no-cache, no-store, must-revalidate";
 		$this->headers["Expires"] = "0";
 	}
 }
